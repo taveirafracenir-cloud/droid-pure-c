40e29569950af8a9c157eedebe316b0d955438a7
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-21 14:18:17 -0300

    Create basics_operations.cpp

---
+++ kernel/cpps_files/basics_operations.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2025–2026 Francenir Inc.
+ * Copyright 2025–2026 DroidPureC Kernel Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+//////////////
+/////////////
+////////////
+int x;        // valor indefinido
+float y;      // valor indefinido
+char letra;   // valor indefinido
+const int maxUsuarios = 100; // não pode mudar depois
+int a = 10;
+int b = 3;
+int soma = a + b;      // 13
+int sub = a - b;       // 7
+int mult = a * b;      // 30
+float div = (float)a / b; // 3.3333
