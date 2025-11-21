7cc7845c94e2048d760e2995b9c2916446c09202
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-21 12:29:29 -0300

    Create types.h

---
+++ include/droidPureC/types.h
@@ -0,0 +1,9 @@
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+typedef unsigned long  u64;
+
+typedef signed char    s8;
+typedef signed short   s16;
+typedef signed int     s32;
+typedef signed long    s64;
