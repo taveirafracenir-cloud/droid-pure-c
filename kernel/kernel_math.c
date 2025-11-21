aedac5c37d2c30fbf7791d9b869bb2b65a993ff5
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-21 13:15:03 -0300

    Create kernel_math.c

---
+++ kernel/kernel_math.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2025–2026 Francenir Inc.
+ * Copyright 2025–2026 DroidPureC Kernel Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
+#define PI 3.14159
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+
+
+float area = PI * raio * raio;
+float area = 3.14159 * raio * raio;
+int x = MAX(5, 10);
+int x = ((5) > (10) ? (5) : (10));
